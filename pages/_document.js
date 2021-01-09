ad212bcda5e2045fa241f0f7c3c2882b92f96030
luke-h1 <luke.howsam@yahoo.com>
2021-01-09 22:22:16 +0000

    commit config files, fix other stuff

---
+++ pages/_document.js
@@ -0,0 +1,28 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="manifest" href="/public/assets/manifest.json" />
+          <link rel="icon" type="image/png" sizes="512x512" href="/public/assets/icons/icon-512.png" />
+          <link rel="apple-touch-icon" type="image/png" sizes="512x512" href="/public/assets/icons/icon-512.png" />
+          <link rel="icon" type="image/png" sizes="192x192" href="/public/assets/icons/icon-192.png" />
+          <link rel="apple-touch-icon" type="image/png" sizes="192x192" href="/public/assets/icons/icon-192.png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument

8f1672b175a7e194a694760d71efe82ef944d38c
luke-h1 <luke.howsam@yahoo.com>
2021-01-09 20:09:39 +0000

    setup styled-components & add global styles

---
+++ styles/Fonts.ts
@@ -0,0 +1,2 @@
+export const PrimaryFont = '"Roboto", sans-serif'
+
---
+++ styles/GlobalStyles.ts
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from 'styled-components'
+import { normalize } from 'polished'
+import { PrimaryFont } from './Fonts'
+import Roboto from '../fonts/Roboto-Regular.ttf'
+
+export const GlobalStyle = createGlobalStyle`
+    ${normalize()}
+    * {
+        box-sizing: border-box;
+        padding: 0;
+        margin: 0;
+    }
+    html {
+        font-size: 16px;
+        box-sizing: border-box;
+        scroll-behavior: smooth;
+    }
+
+    *, *:before *:after {
+        box-sizing: inherit;
+    }
+
+    body { 
+        font-family: ${PrimaryFont};
+        background: #fff;
+    }
+`
